6de4bf99b9549c284bd897ae5a5cf452cc614cab
Antonio Galea <antonio.galea@gmail.com>
2018-10-11 01:50:42 +0200

    fix path

--- acmesystems_build_all_images.sh
+++ acmesystems_build_all_images.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-../gocode/bin/debos acmesystems_common.yaml
-../gocode/bin/debos -t board:roadrunner acmesystems_board.yaml
-../gocode/bin/debos -t board:acqua acmesystems_board.yaml
+gocode/bin/debos acmesystems_common.yaml
+gocode/bin/debos -t board:roadrunner acmesystems_board.yaml
+gocode/bin/debos -t board:acqua acmesystems_board.yaml
 
-../gocode/bin/debos -t arch:armel acmesystems_common.yaml
-../gocode/bin/debos -t arch:armel -t board:aria acmesystems_board.yaml
-../gocode/bin/debos -t arch:armel -t board:arietta acmesystems_board.yaml
+gocode/bin/debos -t arch:armel acmesystems_common.yaml
+gocode/bin/debos -t arch:armel -t board:aria acmesystems_board.yaml
+gocode/bin/debos -t arch:armel -t board:arietta acmesystems_board.yaml
